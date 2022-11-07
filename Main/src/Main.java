7211ed2b5cc47e952b09a8a805749926becdd26d
lukas <lukas.dewachter@student.kuleuven.be>
2022-11-07 14:33:23 +0100

    JSON lib

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,21 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.Iterator;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Object obj = new JSONParser().parse(new FileReader("./input/TOY-20-10.json"));
+        JSONObject jo = (JSONObject) obj;
+        String name = (String)jo.get("name");
+        double weight_duration = (double)jo.get("weight_duration");
+        long horizon = (long)jo.get("horizon");
+        JSONArray ja = (JSONArray) jo.get("jobs");
+        Iterator itr = ja.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr);
+        }
     }
 }
