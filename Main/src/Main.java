22ff1f67d56ee7fb56381754d9f50aa87cd415ae
lukas <lukas.dewachter@student.kuleuven.be>
2022-11-09 11:39:54 +0100

    jobs aanmaken

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,9 +2,13 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
-import java.lang.reflect.Array;
-import java.util.Iterator;
-//eerst een feasable oplossing vormen dan lokaal zoeken om betere oplossingen te zoeken
+import java.util.ArrayList;
+
+/*
+    - eerst een feasable oplossing vormen dan lokaal zoeken om betere oplossingen te zoeken
+    - welke datastructuur voor de oplossing, zien dat oplossing 1x gesaved wordt LinkedList
+    - geen unavailability of setup tijd in de linked list oplossing steken
+*/
 public class Main {
     public static void main(String[] args) throws Exception {
         Object obj = new JSONParser().parse(new FileReader("./input/TOY-20-10.json"));
@@ -13,15 +17,17 @@ public class Main {
         double weight_duration = (double)jo.get("weight_duration");
         long horizon = (long)jo.get("horizon");
         JSONArray ja = (JSONArray) jo.get("jobs");
-        for(int i=0; i<ja.size() ; i++){
-            JSONObject temp = (JSONObject) ja.get(i);
-            long id = (long)temp.get("id");
-            long duration = (long)temp.get("duration");
-            long release_date = (long)temp.get("release_date");
-            long due_date = (long)temp.get("due_date");
-            double earliness_penalty = (double)temp.get("earliness_penalty");
-            double rejection_penalty = (double)temp.get("rejection_penalty");
-            System.out.println(id+" "+duration+" "+release_date+" "+due_date+" "+earliness_penalty+" "+rejection_penalty);
+        List<Job> jobs = new ArrayList<Job>();
+        for (Object o : ja) {
+            JSONObject temp = (JSONObject) o;
+            long id = (long) temp.get("id");
+            long duration = (long) temp.get("duration");
+            long release_date = (long) temp.get("release_date");
+            long due_date = (long) temp.get("due_date");
+            double earliness_penalty = (double) temp.get("earliness_penalty");
+            double rejection_penalty = (double) temp.get("rejection_penalty");
+            Job job = new Job(id, duration, release_date, due_date, earliness_penalty, rejection_penalty);
+            jobs.add(job);
         }
     }
 }
