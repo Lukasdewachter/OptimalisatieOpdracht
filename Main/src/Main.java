6d39e68d98792c1e966ae6009ebab6a3302c0fa7
aminkhatibi <aminkhatibi@icloud.com>
2022-11-13 15:26:47 +0100

    main, org.json

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,9 +1,8 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.json.*;
+import java.io.*;
+import java.util.*;
+import org.json.JSONTokener;
 import org.json.simple.parser.JSONParser;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /*
     - eerst een feasable oplossing vormen dan lokaal zoeken om betere oplossingen te zoeken
@@ -14,44 +13,35 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception {
         Object obj = new JSONParser().parse(new FileReader("./input/TOY-20-10.json"));
-        JSONObject jo = (JSONObject) obj;
-        String name = (String)jo.get("name");
-        double weight_duration = (double)jo.get("weight_duration");
-        long horizon = (long)jo.get("horizon");
+        JSONTokener tokener = new JSONTokener(String.valueOf(obj));
+        JSONObject object = new JSONObject(tokener);
+        String name = object.getString("name");
+        double weight_duration = object.getDouble("weight_duration");
+        int horizon = object.getInt("horizon");
         System.out.println("Test: "+name+", duration "+ weight_duration+", horizon: "+horizon);
-        JSONArray jobsArray = (JSONArray) jo.get("jobs");
+        JSONArray jobsArray = object.getJSONArray("jobs");
         List<Job> jobs = new ArrayList<>();
-        for (Object o : jobsArray) {
-            JSONObject temp = (JSONObject) o;
-            long id = (long) temp.get("id");
-            long duration = (long) temp.get("duration");
-            long release_date = (long) temp.get("release_date");
-            long due_date = (long) temp.get("due_date");
-            double earliness_penalty = (double) temp.get("earliness_penalty");
-            double rejection_penalty = (double) temp.get("rejection_penalty");
+        for (int i = 0; i<jobsArray.length();i++) {
+            JSONObject temp = jobsArray.getJSONObject(i);
+            int id = temp.getInt("id");
+            int duration =  temp.getInt("duration");
+            int release_date = temp.getInt("release_date");
+            int due_date = temp.getInt("due_date");
+            float earliness_penalty = temp.getFloat("earliness_penalty");
+            float rejection_penalty = temp.getFloat("rejection_penalty");
             Job job = new Job(id, duration, release_date, due_date, earliness_penalty, rejection_penalty);
             jobs.add(job);
         }
-        JSONArray unavailability = (JSONArray) jo.get("unavailability");
+        JSONArray unavailability = (JSONArray) object.get("unavailability");
         Unavailability un = new Unavailability(horizon);
-        for(Object o : unavailability){
-            JSONObject temp = (JSONObject)o;
-            long start = (long)temp.get("start");
-            long end = (long)temp.get("end");
+        for(int i=0; i<jobsArray.length();i++){
+            JSONObject temp = jobsArray.getJSONObject(i);
+            int start = temp.getInt("start");
+            int end = temp.getInt("end");
             un.addUnavailable(start,end);
         }
-        JSONArray s = (JSONArray) jo.get("setups");
-        for(Object o : s){
-            JSONObject temp = (JSONObject) o;
-            int[][] setups = new int[jobs.size()][jobs.size()];
-            for(int i=0; i<s.size(); i++){
-                setups[i] = (int[]) temp.get("");
-            }
-            for(int[] t : setups){
-                for(int g : t){
-                    System.out.print(g+" ");
-                }
-            }
-        }
+        int[][] setups = new int[jobs.size()][jobs.size()];
+        JSONArray s = (JSONArray) object.get("setups");
+
     }
 }
--- Main/src/Setup.java
+++ Main/src/Setup.java
@@ -1,6 +1,9 @@
 public class Setup {
-
+    int setupTime;
     public Setup(Job job1, Job job2){
-
+       //
+    }
+    int setupTime(Job job1, Job job2){
+       return 0;
     }
 }
