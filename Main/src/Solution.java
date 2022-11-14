2d8af62d80feae4ba7b6caabb06dc086e62f9505
aminkhatibi <aminkhatibi@icloud.com>
2022-11-14 14:04:13 +0100

    Solution aangepast

--- Main/src/Solution.java
+++ Main/src/Solution.java
@@ -14,39 +14,8 @@ public class Solution {
 
     // Function to schedule the jobs take 2 arguments
     // arraylist and no of jobs to schedule
-    void printJobScheduling(ArrayList<Job> jobs)
-    {
-        // Length of array
-        int n = jobs.size();
+    void printJobScheduling(ArrayList<Job> jobs) {
 
-        // Sort all jobs according to due date
-        jobs.sort(Comparator.comparing(a -> a.getDueDate()));
-
-
-        // To keep track of free time slots
-        boolean result[] = new boolean[];
-
-        // To store result (Sequence of jobs)
-        char job[] = new char[t];
-
-        // Iterate through all given jobs
-        for (int i = 0; i < n; i++) {
-            // Find a free slot for this job (Note that we
-            // start from the last possible slot)
-            for (int j = (int) Math.min(t - 1, jobs.get(i).getReleaseDate() - 1); j >= 0; j--) {
-                // Free slot found
-                if (result[j] == false) {
-                    result[j] = true;
-                    job[j] = (char) jobs.get(i).getId();
-                    break;
-                }
-            }
-        }
-
-        // Print the sequence
-        for (char jb : job)
-            System.out.print(jb + " ");
-        System.out.println();
     }
     boolean isScheduled(Job job){
         if (solution.contains(job)){
